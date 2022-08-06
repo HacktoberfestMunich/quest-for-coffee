6f3e888941a757966048ba130d92bbb4daef0437
Poeschl <Poeschl@users.noreply.github.com>
2022-08-06 15:44:21 +0000

    Deploying to gh-pages from @ Poeschl/space-riddles@d6da49f6e4d00cf0d24db4dced9caa1439217ef3 🚀

--- map/script.js
+++ map/script.js
@@ -1,1 +1,24 @@
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+/* This script will check regulary for flags on solve puzzles. If flags are true the corresponding doors will open. */
+
+const UPDATE_INTERVAL = 30000 //in ms
+
+function getDoorFlags() {
+  return {"riddle-0": true};
+}
+
+function openDoor(name) {
+  WA.room.hideLayer("Doors/" + name);
+}
+
+function checkForNewOpenDoors() {
+  const doorFlags = getDoorFlags();
+  console.debug("Recieved door flags" + JSON.stringify(doorFlags))
+
+  for (const [door, open] of Object.entries(doorFlags)) {
+    if (open) {
+      openDoor(door)
+    }
+  }
+}
+
+setInterval(checkForNewOpenDoors, UPDATE_INTERVAL)
