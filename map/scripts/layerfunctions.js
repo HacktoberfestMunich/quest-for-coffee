0f02ff20ea33b89e38f97a150bdca5315b0e5d6b
Markus Pöschl <Poeschl@users.noreply.github.com>
2022-09-23 20:18:06 +0200

    ♻️ Hopefully show mate message

--- map/scripts/layerfunctions.js
+++ map/scripts/layerfunctions.js
@@ -1,28 +1,30 @@
 const NO_MATE_LAYER = 'noMate';
 const NO_COFFE_LAYER = 'noCoffee';
 
-let shownActionMessage
-
 function emptyMate() {
-  shownActionMessage = WA.ui.displayActionMessage({
-    message: "Kein Mate verfügbar",
-    type: "warning"
+  const emptyMateMessage = WA.ui.displayActionMessage({
+    message: "Kein Mate verfügbar"
   });
+  console.debug("Show Mate message")
+
+  setTimeout(() => {
+    emptyMateMessage.remove();
+  }, 1000)
 }
 
 function emptyCoffee() {
-  shownActionMessage = WA.ui.displayActionMessage({
-    message: "Kein Kaffee verfügbar, nur noch koffeinfrei....",
-    type: "warning"
+  const emptyCoffeeMessage = WA.ui.displayActionMessage({
+    message: "Kein Kaffee verfügbar, nur noch koffeinfrei...."
   });
+  setTimeout(() => {
+    emptyCoffeeMessage.remove();
+  }, 1000)
 }
 
 function initLayersFunctions() {
   WA.room.onEnterLayer(NO_MATE_LAYER).subscribe(() => emptyMate);
-  WA.room.onLeaveLayer(NO_MATE_LAYER).subscribe(() => { shownActionMessage.remove(); });
 
   WA.room.onEnterLayer(NO_COFFE_LAYER).subscribe(() => emptyCoffee);
-  WA.room.onLeaveLayer(NO_COFFE_LAYER).subscribe(() => { shownActionMessage.remove(); });
 }
 
 export { initLayersFunctions };
