60d02d7af2cfeff3f36b89565e1be32190498ada
Markus Pöschl <Poeschl@users.noreply.github.com>
2022-09-23 20:34:22 +0200

    💄 Better message handling

--- map/scripts/layerfunctions.js
+++ map/scripts/layerfunctions.js
@@ -5,25 +5,21 @@ function emptyMate() {
   const emptyMateMessage = WA.ui.displayActionMessage({
     message: "Kein Mate verfügbar"
   });
-  console.debug("Show Mate message")
-
-  setTimeout(() => {
-    emptyMateMessage.remove();
-  }, 1000)
+  console.debug("Show Mate")
 }
 
 function emptyCoffee() {
   const emptyCoffeeMessage = WA.ui.displayActionMessage({
     message: "Kein Kaffee verfügbar, nur noch koffeinfrei...."
   });
-  setTimeout(() => {
-    emptyCoffeeMessage.remove();
-  }, 1000)
 }
 
 function initLayersFunctions() {
   WA.room.onEnterLayer(NO_MATE_LAYER).subscribe(() => { emptyMate(); });
+  WA.room.onLeaveLayer(NO_MATE_LAYER).subscribe(() => { shownActionMessage.remove(); });
+
   WA.room.onEnterLayer(NO_COFFE_LAYER).subscribe(() => { emptyCoffee(); });
+  WA.room.onLeaveLayer(NO_COFFE_LAYER).subscribe(() => { shownActionMessage.remove(); });
 }
 
 export { initLayersFunctions };
