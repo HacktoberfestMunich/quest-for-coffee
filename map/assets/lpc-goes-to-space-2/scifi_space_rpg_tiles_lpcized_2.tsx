cfc7f8f6215a6b9054da4b583b65e93fe811997c
Markus Pöschl <Poeschl@users.noreply.github.com>
2022-09-23 20:26:50 +0200

    🔥 Remove Tiled specific files

--- map/assets/lpc-goes-to-space-2/scifi_space_rpg_tiles_lpcized_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="scifi_space_rpg_tiles_lpcized_2" tilewidth="32" tileheight="32" tilecount="160" columns="20">
- <image source="scifi_space_rpg_tiles_lpcized_2.png" width="640" height="256"/>
-</tileset>
